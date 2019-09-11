cd00aa8680948652e4424cf1769a2a1663a1132b
Neel14-stack <54886608+Neel14-stack@users.noreply.github.com>
2019-09-11 14:06:19 +0530

    Update EXPL1_LED_FLASHER.ino

--- EXP1_LED_FLASHER/EXPL1_LED_FLASHER.ino
+++ EXP1_LED_FLASHER/EXPL1_LED_FLASHER.ino
@@ -1,12 +1,12 @@
 void setup()
 {
-  pinMode(13, OUTPUT);
+  pinMode(6, OUTPUT);
 }
 
 void loop()
 {
-  digitalWrite(13, HIGH);
+  digitalWrite(6, HIGH);
   delay(1000);
-  digitalWrite(13, LOW);
+  digitalWrite(6, LOW);
   delay(1000);
 }
