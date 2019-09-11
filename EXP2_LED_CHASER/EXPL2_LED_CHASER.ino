b93e0481173c436a804517e90f8cad5d30d92c6d
Neel14-stack <54886608+Neel14-stack@users.noreply.github.com>
2019-09-11 10:35:40 -0700

    Update EXPL2_LED_CHASER.ino

--- EXP2_LED_CHASER/EXPL2_LED_CHASER.ino
+++ EXP2_LED_CHASER/EXPL2_LED_CHASER.ino
@@ -1,20 +1,18 @@
-int j=1,i;
+int i,j;
+
 void setup()
 {
- for(int a=0;a<=6;a++)
-  pinMode(a, OUTPUT);
-
-
+for (int i=8;i<=13;i=i+1)
+{ pinMode(i,OUTPUT);}
 }
-
 void loop()
 {
-  i=j-1;
-  digitalWrite(i,HIGH);
- if(i>=6)
- j=0;
- digitalWrite(j,HIGH);
- delay(1000);
- digitalWrite(i,LOW); 
-  j++;
+  for(j=8;j<=13;j=j+1)
+  {
+    digitalWrite(j, HIGH);
+    delay(250);
+    digitalWrite(j, LOW);
+    
+  }
+  
 }
